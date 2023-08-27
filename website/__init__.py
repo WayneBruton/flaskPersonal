f60fd7cd25d5fea9c18d6dfd32eaba501e955c24
Wayne Bruton <waynebruton@icloud.com>
2023-08-27 11:01:10 +0200

    2023/08/27I

--- website/__init__.py
+++ website/__init__.py
@@ -14,9 +14,8 @@ load_dotenv()
 DB_NAME = os.getenv("DB_NAME")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
+
 # if website/uploads folder does not exist, create it
-if not os.path.exists('website/uploads'):
-    os.makedirs('website/uploads')
 
 
 def create_app():
