bfca37b4356c04d738b73b3360adbe378786cdce
Wayne Bruton <waynebruton@icloud.com>
2023-08-29 14:49:55 +0200

    2023/08/29C

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from website import create_app
 
+# create folder website/uploads if not exist
+import os
+
+if not os.path.exists('website/uploads'):
+    os.makedirs('website/uploads')
+
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8010)
+    app.run()
 
 # app.run(debug=True, port=8010)
